7a99dc61ccbac4b1ca5646507579e5ec74d680b6
shaunbuchan <47092882+shaunbuchan@users.noreply.github.com>
2019-03-11 19:50:17 +0000

    Update login.php

--- login.php
+++ login.php
@@ -2,8 +2,8 @@
 session_start();
 if ($_POST['loginButton']) {
     include_once('Connection.php');
-    $username = strip_tags($_POST['username']);
-    $password = strip_tags($_POST['password']);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
     // $pass= md5($password);
 
     $query = "SELECT *  FROM Host where username = '$username' AND password ='$password' LIMIT 1 ";
